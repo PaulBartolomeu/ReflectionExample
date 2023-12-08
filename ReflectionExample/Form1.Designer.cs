a8016d6eff28fb0f6be7d9d94f05641b21da4d5a
Paul Bartolomeu <paul.bartolomeu@razielhealth.com>
2023-12-08 15:36:43 -0500

    commit last

--- ReflectionExample/Form1.Designer.cs
+++ ReflectionExample/Form1.Designer.cs
@@ -31,6 +31,11 @@
             cbObList = new ComboBox();
             txtClassDetails = new TextBox();
             panel1 = new Panel();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // cbObList
@@ -63,17 +68,59 @@
             panel1.TabIndex = 2;
             panel1.TabStop = true;
             // 
+            // button1
+            // 
+            button1.Location = new Point(969, 43);
+            button1.Name = "button1";
+            button1.Size = new Size(139, 42);
+            button1.TabIndex = 3;
+            button1.Text = "&Filter";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(737, 62);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(226, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(675, 158);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(662, 204);
+            dataGridView1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(729, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 20);
+            label1.TabIndex = 6;
+            label1.Text = "DeviceID";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1414, 600);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
             Controls.Add(panel1);
             Controls.Add(txtClassDetails);
             Controls.Add(cbObList);
             Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -83,5 +130,9 @@
         private ComboBox cbObList;
         private TextBox txtClassDetails;
         private Panel panel1;
+        private Button button1;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
+        private Label label1;
     }
 }
--- ReflectionExample/Form1.cs
+++ ReflectionExample/Form1.cs
@@ -1,7 +1,12 @@
 namespace ReflectionExample
 {
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using System.ComponentModel;
+    using System.Data;
     using System.Reflection;
     using System.Text;
+    using System.Linq;
 
     public partial class Form1 : Form
     {
@@ -17,7 +22,7 @@ namespace ReflectionExample
             {
                 cbObList.DataSource = assb.GetTypes().ToList().Where(a => a.FullName != null && a.FullName.Contains(".")).ToList();
             }
-
+            FillGridView();
             instance = this;
 
         }
@@ -27,8 +32,17 @@ namespace ReflectionExample
             var type = assb.GetType(objectname);
             DisplayType(type, objectname);
         }
+        private void FillGridView()
+        {
+            var par = Repo();
+            dynamic content = JsonConvert.DeserializeObject(par);
 
-        private void DisplayType<T>(T obj,string obname)
+            var dd = (from ii in (content["Devices"] as IEnumerable<dynamic>) select new { DeviceId = ii["DeviceId"], Model = ii["Model"], Name = ii["Name"] })
+                .Where(predicate => predicate.DeviceId != null && (string.Empty + predicate?.DeviceId).Contains(((textBox1.Text.Length>0) ?textBox1.Text: (string.Empty + predicate?.DeviceId))))
+                .ToList();
+            dataGridView1.DataSource = ToDataTable(dd);
+        }
+        private void DisplayType<T>(T obj, string obname)
         {
             StringBuilder sb = new StringBuilder();
             Type thetype = typeof(T);
@@ -43,6 +57,99 @@ namespace ReflectionExample
                 if (meth != null) { sb.AppendLine("\tMethod: " + meth.Name); }
             }
             this.txtClassDetails.Text = sb?.ToString();
+        }
+
+
+        private DataTable ToDataTable<T>(IList<T> data)
+        {
+            DataTable table = new DataTable();
+
+            //special handling for value types and string
+            if (typeof(T).IsValueType || typeof(T).Equals(typeof(string)))
+            {
+
+                DataColumn dc = new DataColumn("Value");
+                table.Columns.Add(dc);
+                foreach (T item in data)
+                {
+                    DataRow dr = table.NewRow();
+                    dr[0] = item;
+                    table.Rows.Add(dr);
+                }
+            }
+            else
+            {
+                PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+                foreach (PropertyDescriptor prop in properties)
+                {
+                    table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+                }
+                foreach (T item in data)
+                {
+                    DataRow row = table.NewRow();
+                    foreach (PropertyDescriptor prop in properties)
+                    {
+                        try
+                        {
+                            row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                        }
+                        catch (Exception ex)
+                        {
+                            row[prop.Name] = DBNull.Value;
+                        }
+                    }
+                    table.Rows.Add(row);
+                }
+            }
+            return table;
+        }
+        private string Repo()
+        {
+            return $@"{{
+
+Devices:[
+        {{
+            ""Category"": ""DM"",
+            ""DeviceId"": ""@E-c44268000042"",
+            ""Model"": ""HD-PS622"",
+            ""Name"": ""HD-PS622-C44268000042"",
+            ""RebootReason"": ""poweron""
+        }},
+        {{
+            ""Category"": ""DM"",
+            ""DeviceId"": ""@E-FD123412341"",
+            ""Model"": ""JU-OEIUW"",
+            ""Name"": ""JU-OEIUW-FD123412341"",
+            ""RebootReason"": ""poweron""
+        }},
+        {{
+            ""Category"": ""DM"",
+            ""DeviceId"": ""@E-SD13241234"",
+            ""Model"": ""IU-YIYASD"",
+            ""Name"": ""IU-YIYASD-SD13241234"",
+            ""RebootReason"": ""poweron""
+        }},
+        {{
+            ""Category"": ""DM"",
+            ""DeviceId"": ""@E-YT342352455"",
+            ""Model"": ""TR-EOIUWQ"",
+            ""Name"": ""TR-EOIUWQ-YT342352455"",
+            ""RebootReason"": ""poweron""
+        }},
+        {{
+            ""Category"": ""DM"",
+            ""DeviceId"": ""@E-ER1241234"",
+            ""Model"": ""RW-DAKLSJDAL"",
+            ""Name"": ""RW-DAKLSJDAL-ER1241234"",
+            ""RebootReason"": ""poweron""
+        }}
+		]
+}}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FillGridView();
         }
     }
 
